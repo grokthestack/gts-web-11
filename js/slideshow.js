57075be8b818f94c0a8bf6cd9559797ea9ae5ec3
primeviking <sara.evensen@gmail.com>
2013-03-23 13:27:54 -0700

    Added javascript for the tabs (still returning an error) Formatted the navbar loosely

--- js/slideshow.js
+++ js/slideshow.js
@@ -22,3 +22,28 @@ Eve.scope('#slideshow', function(){
 	});
 
 });
+
+Eve.scope('#tabs', function(){
+	
+	var parent = this;
+
+	this.listen('.tabbed_nav li', 'click', function(e) {
+		//Keeps link from being changed
+		e.stop();
+		//Remove class 'active' from previously active tab
+		this.find('.active').removeClass('active');		
+		//defines the tabName as the href of the clicked li
+		var tabName = e.target.getElementsByTagName('a')[0].getAttribute('href').replace('/^#/', '');
+		//Add class to the active tab
+		//parent.find('#' + tabName).addClass('active');
+		for (var i = 0; i < $$(".tabbed_nav li").length; i++) {
+			if ($$(".tabbed_nav li")[i].getElementsByTagName('a')[0].getAttribute('href') === "#" + tabName) {
+				$$(".tabbed_nav li")[i].addClass('active');
+			}
+		}
+
+		//Adds class to the active tab content
+		parent.find('.tab_content .' + tabName).addClass('active');
+	});
+
+});
